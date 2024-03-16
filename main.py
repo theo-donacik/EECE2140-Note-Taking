23337e3651b37d7dae9822883b9234cad72e3802
The0919 <theodonacik@gmail.com>
2024-03-16 13:44:47 -0400

    Added notemanager for managing the loading, creation, and editing of notes for each session

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+import os
+
 class Note():
-  def __init__(self):
+  def __init__(self, fileName):
     self.contents = ""
-    self.fileName = ""
+    self.fileName = fileName
 
   def edit(self, contents):
     self.contents = contents
@@ -15,7 +17,49 @@ class Note():
     self.fileName = fileName
     self.save()
 
+  def load(self):
+    f = open(self.fileName, "r")
+    self.contents = f.read()
+    f.close()
+
+class NoteManager:
+  def __init__(self):
+    configFile = ".notes"
+    notes = []
+    if os.path.isfile(configFile):
+      f = open(configFile, "r")
+      for line in f:
+        line = line.strip('\n')
+        note = Note(line)
+        note.load()
+        notes.append(note)
+      f.close()
+    else:
+      f = open(configFile, "x")
+      f.close()
+
+    self.notes = notes
+    self.configFile = configFile
+
+  def addToConfig(self, note):
+    f = open(self.configFile, "a")
+    f.write(f'{note.fileName}\n')
+    f.close()
+
+  def newNote(self, fileName, contents):
+    note = Note(fileName)
+    note.edit(contents)
+    note.save()
+    self.notes.append(note)
+    self.addToConfig(note)
+    
+  def editNote(self, fileName, newContents):
+    note = next(filter(lambda note: note.fileName == fileName, self.notes))
+    note.edit(newContents)
+    note.save()
+
 def main():
-  return
+  manage = NoteManager()
+  manage.newNote("note1.txt", "hi")
 
 main()
